f3dd85540d6613d6482a16c40e2a276cd20b53c4
Tanay <tanay.joshi9@gmail.com>
2022-05-15 22:26:18 +0530

    feat: navbar component

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import { Banner, Featured } from "../../components/homepage";
+import { Navbar } from "../../components/navigation";
 
 const Homepage = () => {
   return (
     <div>
+      <Navbar />
       <Banner />
       <section className="text-left m-m">
-        <h3 className="text-dark my-s">Must Watch</h3>
+        <h3 className="text-dark m-s">Must Watch</h3>
         <Featured />
       </section>
     </div>
