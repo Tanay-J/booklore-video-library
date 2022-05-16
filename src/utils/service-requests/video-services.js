ca58483d013f21aa1f4c16079c30271a16d008e8
Tanay <tanay.joshi9@gmail.com>
2022-05-16 22:09:30 +0530

    refactor: use IIFE inside useEffect for async functions

--- src/utils/service-requests/video-services.js
+++ src/utils/service-requests/video-services.js
@@ -4,21 +4,23 @@ import axios from "axios";
 const getVideos = (category) => {
   const [videoList, setVideoList] = useState([]);
 
-  useEffect(async () => {
-    try {
-      const {
-        data: { videos },
-      } = await axios.get("/api/videos");
+  useEffect(() => {
+    (async () => {
+      try {
+        const {
+          data: { videos },
+        } = await axios.get("/api/videos");
 
-      if (category) {
-        const filteredVideos = videos.filter(
-          (video) => video.category === category
-        );
-        setVideoList(filteredVideos);
-      } else setVideoList(videos);
-    } catch (error) {
-      console.log(error);
-    }
+        if (category) {
+          const filteredVideos = videos.filter(
+            (video) => video.category === category
+          );
+          setVideoList(filteredVideos);
+        } else setVideoList(videos);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, [category]);
 
   return { videoList };
@@ -27,15 +29,17 @@ const getVideos = (category) => {
 const getCategories = () => {
   const [categories, setCategories] = useState([]);
 
-  useEffect(async () => {
-    try {
-      const {
-        data: { categories },
-      } = await axios.get("/api/categories");
-      setCategories(categories);
-    } catch (error) {
-      console.log(error);
-    }
+  useEffect(() => {
+    (async () => {
+      try {
+        const {
+          data: { categories },
+        } = await axios.get("/api/categories");
+        setCategories(categories);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return { categories };
