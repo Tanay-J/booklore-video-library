f061cf825a1b2bf6c60d37d675250b41a099c84f
Tanay <tanay.joshi9@gmail.com>
2022-06-29 10:44:17 +0530

    refactor: recommeded video suggestions

--- src/components/homepage/Featured.jsx
+++ src/components/homepage/Featured.jsx
@@ -6,6 +6,8 @@ import styles from "./homepage.module.css";
 const Featured = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { videoList } = getVideos("", setIsLoading);
+  videoList.sort((video1, video2) => video2.views - video1.views);
+
   return (
     <section className={`${styles.videolist_container}`}>
       {videoList.slice(0, 4).map((video) => (
--- src/components/singleVideo/RecommendedSidebar.jsx
+++ src/components/singleVideo/RecommendedSidebar.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { getVideos } from "utils/service-requests/video-services";
 import { RecommendedVideoCard } from "./RecommendedVideoCard";
 import styles from "./singleVideo.module.css";
 const RecommendedSidebar = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { videoList } = getVideos("", setIsLoading);
-
+  const {
+    state: { currentVideo },
+  } = useLocation();
+  const recommendedVideoList = videoList.filter(
+    (video) =>
+      video.category === currentVideo.category && video._id !== currentVideo._id
+  );
   return (
     <aside>
       <ul className="mx-xl">
         <p className="text-dark text-s font-bold">Recommended</p>
-        {videoList.slice(0, 3).map((video) => {
+        {recommendedVideoList.slice(0, 3).map((video) => {
           return (
             <li
               className={`${styles.card_container_no_button} my-xs`}
